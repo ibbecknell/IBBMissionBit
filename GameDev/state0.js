3c4cdf3e9a7c355b6e5b92b13e2db03c306c3a1e
ibbecknell <missionbit@Missions-MacBook-Air.local>
2016-02-25 18:38:11 -0800

    added Phaser Tutorial 1

---
+++ GameDev/state0.js
@@ -0,0 +1,19 @@
+var demo = {};
+demo.state0 = function(){};
+
+
+demo.state0.prototype = {
+	preload: function(){
+		game.load.image('trees', 'assets/background/treeBG.png');
+		game.load.spritesheet('adam', 'assets/spritesheets/adamSpritesheet.png');
+	},
+
+	create: function(){
+
+	},
+
+	update: function(){
+
+	}
+
+};
